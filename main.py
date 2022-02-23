bde114a4b064c0bef89cd7726567ad4d0ed0b03c
Azri <azkaerial@gmail.com>
2022-02-23 16:39:55 +0800

    Project

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
 
+
+
+
+
+
+
+
+
+
+
+
+
+import scrapy
+
+class NewSpider(scrapy.Spider):
+   name = "new_spider"
+   start_urls = ["https://brickset.com/sets/year-2011"]
+
+   def parse(self, response):
+      css_selector = 'img'
+      for x in response.css(css_selector):
+         newsel = '@src'
+         yield {
+                 'Image Link': x.xpath(newsel).extract_first(),
+         }
